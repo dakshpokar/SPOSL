460171640260f664385c4d21e3dcaad2cf747299
Daksh Pokar <dakshpokar@gmail.com>
2019-04-17 23:55:29 -0700

    Adde PRac

---
+++ pracSPOSL/A1/src/ICLine.java
@@ -0,0 +1,26 @@
+
+public class ICLine {
+	private String code;
+	private String type;
+	public ICLine() {
+		// TODO Auto-generated constructor stub
+	}
+	public ICLine(String code, String type) {
+		super();
+		this.code = code;
+		this.type = type;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
